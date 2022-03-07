c62f234f1f2ece19e760b97c45255170d703816c
Jakub Aleksander <jakubaleksander@iMac.local>
2022-03-07 17:09:01 +0000

    added gatsby bowser and gatsby ssr for themeprovider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,7 @@
 import React from "react"
-import {ThemeProvider} from "styled-components"
+import { ThemeProvider } from "styled-components"
 import Theme from "./src/themes/theme"
 
 export const wrapRootElement = ({ element }) => (
-    <ThemeProvider theme={Theme}>
-        {element}
-    </ThemeProvider>
+    <ThemeProvider theme={Theme}>{element}</ThemeProvider>
 )
